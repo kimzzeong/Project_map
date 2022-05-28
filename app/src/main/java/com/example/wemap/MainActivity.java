d4cd5e6f6e86c27a629fe5d455ed50b6ec87d8fa
kimzzeong <74105034+kimzzeong@users.noreply.github.com>
2022-05-29 00:30:44 +0900

    map

---
+++ app/src/main/java/com/example/wemap/MainActivity.java
@@ -0,0 +1,96 @@
+package com.example.wemap;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FusedLocationProviderClient client;
+    SupportMapFragment mapFragment;
+    FloatingActionButton my_location_btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        my_location_btn = findViewById(R.id.my_location);
+        getSupportActionBar().hide();
+        mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+
+        client = LocationServices.getFusedLocationProviderClient(this);
+
+        permissionLocation();
+
+        /*
+        버튼 누르면 내 위치로 이동
+        */
+        my_location_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                permissionLocation();
+            }
+        });
+    }
+
+    public void permissionLocation(){
+        //위치 권한 체크
+        if (ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            //권한 O
+            Task<Location> task = client.getLastLocation();
+            task.addOnSuccessListener(new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    if (location != null){
+
+                        mapFragment.getMapAsync(new OnMapReadyCallback() {
+                            @Override
+                            public void onMapReady(@NonNull GoogleMap googleMap) {
+                                LatLng latLng = new LatLng(location.getLatitude(),location.getLongitude());
+                                //마커
+                                MarkerOptions options = new MarkerOptions().position(latLng)
+                                        .title("현위치");
+                                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,18));
+                                googleMap.addMarker(options);
+                            }
+                        });
+                    }
+                }
+            });
+        }else{
+            //권한 X
+            //퍼미션 요청
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},44);
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 44) {
+            permissionLocation();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/wemap/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.example.wemap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        getSupportActionBar().hide(); // 액션바 가리기
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },2000); //2초후에 splash화면에서 main으로 전환
+    }
+}
