15f0058a4af9fa8d696eb7b3dfe2b2cf45ca4a90
kimzzeong <74105034+kimzzeong@users.noreply.github.com>
2022-05-29 13:56:09 +0900

    camera

--- app/src/main/java/com/example/wemap/SplashActivity.java
+++ app/src/main/java/com/example/wemap/SplashActivity.java
@@ -20,7 +20,7 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(getApplicationContext(), GuideActivity.class);
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
                 finish();
             }
